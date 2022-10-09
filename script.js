7ecf1e27dee55fc3c0022fb439ebe5050d1ada73
Maaz7769 <52111360+Maaz7769@users.noreply.github.com>
2022-10-09 21:46:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ form.addEventListener('click', (e) => {
 
 let searchBtn = document.querySelector('#btn')
 searchBtn.addEventListener('click', () => {
-    let query = textBox.value
+    let query = textBox.value.tolowerCase()
     root.innerHTML=""
     next.style.display = 'none'
     prev.style.display = 'none'
